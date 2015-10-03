597b3da3c3755286cec7320c951715c3a5fa84d0
GPierre <pierregthr@gmail.com>
2015-10-03 19:41:31 -0400

    Depot initial

---
+++ src/main/java/LibreTaxi/LibreTaxiApplication.java
@@ -0,0 +1,12 @@
+package LibreTaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreTaxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibreTaxiApplication.class, args);
+    }
+}
